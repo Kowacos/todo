757ce8df5a704a190d48574163e83e852c8f80bd
Kowacos <precekmi@gmail.com>
2025-09-11 19:27:42 +0200

    add DB

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ console.log('JavaScript se načetl úspěšně!');
 
 // Import a inicializace PocketBase klienta
 import PocketBase from 'https://cdn.jsdelivr.net/npm/pocketbase@0.21.1/dist/pocketbase.es.mjs';
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://a60fd21464f9.ngrok-free.app');
 
 // Čekáme na načtení DOM
 document.addEventListener('DOMContentLoaded', function() {
